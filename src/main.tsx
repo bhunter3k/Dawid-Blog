7c23a62106cd029bed686b8840b6eeec98a6ba14
Dawid Klos <74927650+bhunter3k@users.noreply.github.com>
2024-05-12 17:03:26 +0100

    test 4

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./css/mainStyle.css";
-
-const router = createHashRouter([
-	{
-		path: "/*",
-		element: <App />,
-	},
-]);
-
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-// 	<React.StrictMode>
-// 		<HashRouter>
-// 			<App />
-// 		</HashRouter>
-// 	</React.StrictMode>
-// );
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<HashRouter>
+			<App />
+		</HashRouter>
 	</React.StrictMode>
 );
