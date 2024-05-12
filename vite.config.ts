e3319e345d9a9988c0378ee9ad2dcb87c87e91e5
Dawid Klos <74927650+bhunter3k@users.noreply.github.com>
2024-05-12 16:20:35 +0100

    Implementing GitHub pages functionality

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: 5050,
-		proxy: {
-			"/api": "http://localhost:5051/",
-		},
-	},
+	base: "/Dawid-Blog/",
 });
