e832c37c33abd17f4e3b9961817670853056ee2d
Dawid Klos <74927650+bhunter3k@users.noreply.github.com>
2024-05-14 02:39:25 +0100

    Major changes to css in progress

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,13 @@
+import dawidPhoto from "../../assets/dawidPhoto.jpg";
+
 export const Home = () => {
 	return (
 		<>
 			<h1>Home Page</h1>
 			<hr />
 			<p>test</p>
+
+			<img src={dawidPhoto} alt="Dawid's Blog Logo" id="blogLogo" loading="eager" />
 		</>
 	);
 };
